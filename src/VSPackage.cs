d24b5528d3ee0cd66aa3646ffb96c63e603993e6
Mads Kristensen <post@madskristensen.net>
2018-03-27 11:22:02 -0700

    Updated to asyncpackage with background load

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -8,12 +8,12 @@ using Task = System.Threading.Tasks.Task;
 namespace BrowserSync
 {
     [Guid(PackageGuids.guidBrowserSyncPackageString)]
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideOptionPage(typeof(Options), "Web", Vsix.Name, 101, 102, true, new string[0], ProvidesLocalizedCategoryName = false)]
-    [ProvideAutoLoad(ActivationContextGuid)]
-    [ProvideUIContextRule(ActivationContextGuid, Vsix.Id,
+    [ProvideAutoLoad(_activationContextGuid, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideUIContextRule(_activationContextGuid, Vsix.Id,
         "WAP | WebSite | ProjectK | DotNetCoreWeb",
         new string[] {
             "WAP",
@@ -29,7 +29,7 @@ namespace BrowserSync
         })]
     public sealed class VSPackage : AsyncPackage
     {
-        private const string ActivationContextGuid = "{4b6c8d76-4918-45ab-9b26-8f246c1773ab}";
+        private const string _activationContextGuid = "{4b6c8d76-4918-45ab-9b26-8f246c1773ab}";
 
         public static Options Options
         {
@@ -39,13 +39,12 @@ namespace BrowserSync
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            Options = (Options)GetDialogPage(typeof(Options));
             var commandService = await GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
 
-            if (commandService != null)
-            {
-                EnableSyncCommand.Initialize(this, commandService);
-            }
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            Options = (Options)GetDialogPage(typeof(Options));
+            EnableSyncCommand.Initialize(this, commandService);
         }
     }
 }
